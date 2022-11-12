69575487e840d83c4763910b775547eea63b2249
Lazar Petru <adrian_petru72@yahoo.com>
2022-11-12 14:54:36 +0200

    device orientation example v0.4

--- orientation/script.js
+++ orientation/script.js
@@ -4,11 +4,11 @@ ctx = canvas.getContext('2d')
 var width = canvas.width
 var height = canvas.height
 
-window.addEventListener('orientationchange', orientationChange)
+window.addEventListener('deviceorientation', orientationChange)
 
 function orientationChange(event) {
 	var angle = event.gamma
 
 	ctx.fillStyle = 'black'
-	ctx.fillRect(0, 0, width, height * ((angle - 90) / 200 + 1))
+	ctx.fillRect(0, 0, width, height * ((angle - 90) / 200.0 + 1))
 }
